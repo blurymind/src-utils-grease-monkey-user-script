a7f0cd97177c2238e99edfc6ef3d5704b5cf6b96
Todor Imreorov <blurymind@gmail.com>
2025-12-17 10:36:47 +0000

    Create src-utils.user.js

---
+++ src-utils.user.js
@@ -0,0 +1,218 @@
+// ==UserScript==
+// @name         Source Utils
+// @namespace    blurymind
+// @version      2025-12-10
+// @description  copies to clipboard all loaded video and image src links
+// @author       blurymind
+// @match        https://civitai.com/*
+// @include      https://civitai.com/*
+// @include      https://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=civitai.com
+// @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
+// @grant    GM_setClipboard
+// ==/UserScript==
+
+class Toast {
+  constructor(message,color,time){
+    this.message = message;
+    this.color = color;
+    this.time = time;
+    this.element = null;
+    var element = document.createElement('div');
+    element.className = "toast-notification";
+    this.element = element;
+    var countElements = document.getElementsByClassName("toast-notification");
+      element.style = `
+   min-width: 400px;
+  height:80px;
+  background-color:white;
+  border-radius: 10px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  top:0;
+  left: 50%;
+  margin-top:100px;
+  margin-left:0;
+  position:fixed;
+  z-index:1;
+  display:flex;
+  flex-direction:row;
+  margin-top: ${(countElements.length *100)}px;
+  background-color:${this.color};
+  z-index: 999999;
+      `
+    var mess = document.createElement("div");
+    mess.className = "message-container";
+    mess.style = `
+      width:80%;
+  padding-top:13px;
+  padding-left: 20px;
+  font-family:'Roboto';
+  color:white;
+    `
+    mess.textContent = this.message;
+    element.appendChild(mess);
+    var close = document.createElement("div");
+    close.className = "close-notification";
+      close.style = `
+      width:20%;
+      `
+    var icon = document.createElement("i");
+    icon.className = "lni lni-close";
+    icon.style = `
+      padding-top:15px;
+  padding-left:5px;
+  font-weight:900;
+  color:white;
+  cursor:pointer;
+    `
+    close.appendChild(icon);
+    element.append(close);
+    document.body.appendChild(element);
+    setTimeout(function() {
+      element.remove();
+    }, this.time);
+    close.addEventListener("click",()=>{
+      element.remove();
+    })
+  }
+}
+const ToastType = {
+  Danger : "#eb3b5a",
+  Warning: "#fdcb6e",
+  Succes : "#00b894",
+}
+
+const copyClip = (text, toast) => {
+        navigator.clipboard.writeText(text).then(
+            () => {
+                 new Toast(toast,ToastType.Succes,2000);
+            },
+            () => {
+                new Toast(`Failed to copy ${text}`,ToastType.Danger,2000);
+            },
+        );
+}
+const copyLinks = (type='video/mp4')=>{
+    if(type.startsWith('video')){
+        const sources = [...document.querySelectorAll('source')]
+        .filter(item=>item.type.startsWith('video/mp4'))
+        .map(item=> item.src);
+        console.log({sources})
+        copyClip(sources.join(' '), `Copied visible ${type}: ${sources.length} links`)
+    } else {
+        const sources = [...document.querySelectorAll('img')]
+        .map(item=> item.src);
+        console.log({sources})
+        copyClip(sources.join(' '), `Copied visible ${type}: ${sources.length} links`)
+    }
+    //GM_setClipboard (sources.join(' '));
+}
+
+const getRoot = () => document.getElementById('main') ?? document.body.querySelector("main") ?? document.body;
+const createButton = (name='Copy src', type='video', offset = 10) => {
+    const button = document.createElement('button');
+    button.innerText = name;
+    button.id = name;
+    button.style = `
+    position: fixed;
+    z-index: 999;
+    background: #000000e0;
+    color: yellow;
+    padding: 2px;
+    border-radius: 3px;
+    right: 10px;
+    opacity: 0.7;
+    top: ${100 + (offset|| 0)}px;
+    `;
+
+    button.addEventListener('click', ()=> {
+        copyLinks(type)
+    })
+    const root = getRoot();
+    console.log({root})
+    root.appendChild(button);
+}
+
+const createCopyButton = (imageElement) => {
+    const isVideo = imageElement.tagName === "SOURCE"
+    imageElement.style.position = "relative"
+    const button = document.createElement('button');
+    button.innerText = isVideo ? "v" : " c ";
+    button.title = imageElement.src;
+    button.className = "copyButt"
+    button.style = `
+    position: absolute;
+    z-index: 999;
+    background: #000000e0;
+    color: yellow;
+    border-radius: 3px;
+    padding: 5px;
+    opacity: 0.7;
+    right: 2px;
+    top: 2px;
+    border: none;
+    display: none;
+    `;
+
+    button.addEventListener('click', (e)=> {
+        e.stopPropagation()
+        e.preventDefault()
+        copyClip(imageElement.src, `Copied ${imageElement.src}`)
+    })
+    const attachTo = isVideo ? imageElement.parentElement.parentElement: imageElement.parentElement;
+    //const size = imageElement.getBoundingClientRect()
+    if(attachTo.querySelector(".copyButt")){
+        return;
+    }
+    attachTo.addEventListener('pointerenter', ()=>{
+        console.log("enter")
+        button.style.display = "block"
+    })
+    attachTo.addEventListener('pointerleave', ()=>{
+        console.log("exit")
+    button.style.display = "none"
+    })
+    attachTo.style.filter = "none";
+    attachTo.appendChild(button);
+}
+
+setTimeout(()=>{
+    createButton('copy mp4 urls', 'video/mp4', 0)
+    createButton('copy webm urls', 'video/webm', 30)
+    createButton('copy image urls', 'image', 60)
+}, 5000)
+
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === "childList") {
+      //console.log("A child node has been added or removed.", mutation);
+        setTimeout(()=> {
+            const foundSources = mutation.target.querySelectorAll("source")
+                //console.log({foundSources})
+            if(foundSources){
+                const sources = Array.from(foundSources)
+                //console.log({sources})
+                sources.filter(item=>item.type === "video/mp4").forEach(createCopyButton)
+            }
+        }, 500)
+
+    } else if (mutation.type === "attributes") {
+      //console.log(`The ${mutation.attributeName} attribute was modified.`, mutation);
+        if(mutation.attributeName === "src"){
+            createCopyButton(mutation.target)
+        }
+    }
+  }
+};
+const observer = new MutationObserver(callback);
+
+setTimeout(()=>{
+    const config = { attributes: true, childList: true, subtree: true };
+    const root = getRoot();
+    observer.observe(root, config);
+    document.querySelectorAll("img").forEach(createCopyButton)
+    const sources = Array.from(document.querySelectorAll("source"))
+    //console.log({sources})
+    sources.filter(item=>item.type === "video/mp4").forEach(createCopyButton)
+}, 7000)
